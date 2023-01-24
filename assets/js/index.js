a83b27b18388d585d4c05b68f2af28d7cb4eb5f7
Raul Diamantino <rauldiamantino25@gmail.com>
2023-01-24 00:40:01 -0300

    Feat: Funções para fechar demais details ao clicar em detail específico

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,43 @@
-// botão fechar
+/* ---- constantes ---- */
 const $btnFechar = document.querySelector('.btn-fechar-details')
+const $todosDetails = document.querySelectorAll('details')
 
+/* ---- funções ---- */
 
-// funções
-
+// fecha detail Ordenar através do botão no responsivo
 const fecharTagDetailsOrdenar = () => {
   const detailsOrdenar = document.querySelector('.ordenar')
 
   detailsOrdenar.removeAttribute('open')
 }
 
+// fecha demais details ao clicar em uma
+const fecharDemaisDetails = ($detailClicada) => {
+  $detailClicada.addEventListener('click', () => {
+    $todosDetails.forEach($detail => {
+      if ($detail !== $detailClicada) {
+        $detail.removeAttribute('open')
+      }
+    })
+  })
+}
+
+// fecha detail ao clicar em qualquer lugar da tela - exceto responsivo
+const fecharDetailClicouFora = (e) => {
+  const alvo = e.srcElement.nodeName == "SUMMARY"   
+
+  $todosDetails.forEach($detail => {
+    if ($detail !== alvo) {
+      $detail.removeAttribute('open')
+    }
+  })
+
+}
+
+
+
+// Chamando as funções
 $btnFechar.addEventListener('click', () => fecharTagDetailsOrdenar())
+$todosDetails.forEach(fecharDemaisDetails)
+document.addEventListener('click', (e) => { fecharDetailClicouFora(e) })
+
